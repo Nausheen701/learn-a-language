705dbec24595c7580ac61e35d84ec39f030a0166
Nausheen Akhter <nausheen701@gmail.com>
2021-04-17 02:44:10 +0300

    instructor pages

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -3,13 +3,23 @@ class Instructor < ApplicationRecord
   has_many :courses
   has_many :students, through: :courses
 
-  validates :first_name, :last_name, :username, presence: true
-  validates :username, uniqueness: true
-  validates :password, length: { in: 6..14}
+  validates :first_name, :last_name, presence: true
+  # validates :username, uniqueness: true
+  validates :password, length: {in: 6..14} 
+  
   validates :bio, length: { maximum: 500}
 
   def to_s
     self.first_name + " " + self.last_name
+  end
+
+   # custom method 
+
+   def is_email?
+    if !email.match(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i)
+      errors.add(:email, "Please enter a valid email.")
+    end 
+
   end
 
   # reload! => reload console with updated code
